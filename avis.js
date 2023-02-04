86fdca3b4d0e0141c062e291082a4edf12621e5b
ClaireRuysschaert <netclaire@hotmail.fr>
2023-02-04 18:53:56 +0100

    Enregistrer dans le navigateur (localStorage)

--- avis.js
+++ avis.js
@@ -1,21 +1,28 @@
 export function ajoutListenersAvis() {
-    const piecesElements = document.querySelectorAll(".fiches article button");
 
-    for (let i = 0; i < piecesElements.length; i++) {
-        piecesElements[i].addEventListener("click", async function (event) {
-            const id = event.target.dataset.id;
-            const reponse = await fetch(
-                `http://localhost:8081/pieces/${id}/avis`
-            );
-            const avis = await reponse.json();
-            const pieceElement = event.target.parentElement;
-            const avisElement = document.createElement("p");
-            for (let i=0; i<avis.length;i++) {
-               avisElement.innerHTML += `<b>${avis[i].utilisateur}:</b> ${avis[i].commentaire} <br>`;
-            }
-            pieceElement.appendChild(avisElement);
-        });
-    }
+   const piecesElements = document.querySelectorAll(".fiches article button");
+
+   for (let i = 0; i < piecesElements.length; i++) {
+
+    piecesElements[i].addEventListener("click", async function (event) {
+
+       const id = event.target.dataset.id;
+       const reponse = await fetch("http://localhost:8081/pieces/" + id + "/avis");
+       const avis = await reponse.json();
+       window.localStorage.setItem(`avis-piece-${id}`, JSON.stringify(avis))
+       const pieceElement = event.target.parentElement;
+       afficherAvis(pieceElement, avis)
+    });
+
+   }
+}
+
+export function afficherAvis(pieceElement, avis){
+   const avisElement = document.createElement("p");
+   for (let i = 0; i < avis.length; i++) {
+       avisElement.innerHTML += `<b>${avis[i].utilisateur}:</b> ${avis[i].commentaire} <br>`;
+   }
+   pieceElement.appendChild(avisElement);
 }
 
 export function ajoutListenerEnvoyerAvis() {
@@ -24,18 +31,19 @@ export function ajoutListenerEnvoyerAvis() {
    event.preventDefault();
    // Création de l’objet du nouvel avis.
    const avis = {
-      pieceId: parseInt(event.target.querySelector("[name=piece-id]").value),
-      utilisateur: event.target.querySelector("[name=utilisateur").value,
-      commentaire: event.target.querySelector("[name=commentaire]").value,
-      nbEtoiles: parseInt(envent.target.querySelector("[name=nbEtoiles]").value)
+       pieceId: parseInt(event.target.querySelector("[name=piece-id]").value),
+       utilisateur: event.target.querySelector("[name=utilisateur").value,
+       commentaire: event.target.querySelector("[name=commentaire]").value,
+       nbEtoiles: parseInt(event.target.querySelector("[name=nbEtoiles]").value)
    };
    // Création de la charge utile au format JSON
    const chargeUtile = JSON.stringify(avis);
    // Appel de la fonction fetch avec toutes les informations nécessaires
    fetch("http://localhost:8081/avis", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: chargeUtile
+       method: "POST",
+       headers: { "Content-Type": "application/json" },
+       body: chargeUtile
    });
    });
+   
 }
